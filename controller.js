cf8f2b1f3d4f485c53675c95d1c0da3f365c2172
akhil <akhilsabu8702@gmail.com>
2023-10-19 16:02:50 +0530

    added

--- controller.js
+++ controller.js
@@ -2,8 +2,14 @@ import schema from './user.model.js';
 
 export function addTask(req,res)
 {
+
     const {task}=req.body;
     console.log(task);
     res.status(201).send(schema.create({task}))
-    res.end();
+  
+}
+export async function listTask(req,res)
+{
+    const data = await schema.find();
+    res.status(200).send(data);
 }
--- router.js
+++ router.js
@@ -4,5 +4,7 @@ import * as controller from './controller.js'
 const router=Router();
 
 router.route("/add").post(controller.addTask);
+router.route("/list").get(controller.listTask);
+
 
 export default router;
